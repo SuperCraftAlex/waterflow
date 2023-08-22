f99bb5eed8d1982c642189dcb2d9b868d3741633
alex_s168 <63254202+SuperCraftAlex@users.noreply.github.com>
2023-08-22 15:24:57 +0200

    conditions

--- wf.py
+++ wf.py
@@ -33,6 +33,8 @@ def exec_stack(block, stack):
   global vars
   global funs
 
+  cond = 1.0
+
   # sp points to the current top element in the stack
   sp = len(stack) - 1
 
@@ -42,6 +44,70 @@ def exec_stack(block, stack):
     inst = inst.strip().split(" ")
     args = inst[1:]
     inst = inst[0]
+
+    if inst == "sec":
+      if len(args) != 0:
+        print("too many arguments for sec instruction!")
+        continue
+      cond = True
+      continue
+
+    if inst == "clc":
+      if len(args) != 0:
+        print("too many arguments for clc instruction!")
+        continue
+      cond = False
+      continue
+
+    if inst == "puc":
+      if len(args) != 0:
+        print("too many arguments for puc instruction!")
+        continue
+      sp += 1
+      stack[sp] = cond
+      continue
+
+    if inst == "poc":
+      if len(args) != 0:
+        print("too many arguments for poc instruction!")
+        continue
+      if sp < 0:
+        print("stack underflow! -> end stack-block")
+        continue
+      cond = float(stack[sp])
+      sp -= 1
+      continue
+
+    if inst == "and":
+      # cond = cond and stack
+      if len(args) != 0:
+        print("too many arguments for and instruction!")
+        continue
+      if sp < 0:
+        print("stack underflow! -> end stack-block")
+        continue
+      cond = int(cond) == 1 and int(stack[sp]) == 1
+      sp -= 1
+      continue
+
+    if inst == "or":
+      # cond =q cond or stack
+      if len(args) != 0:
+        print("too many arguments for and instruction!")
+        continue
+      if sp < 0:
+        print("stack underflow! -> end stack-block")
+        continue
+      cond = 1.0 if (int(cond) == 1 or int(stack[sp]) == 1) else 0.0
+      sp -= 1
+      continue
+
+    if inst == "not":
+      if len(args) != 0:
+        print("too many arguments for not instruction!")
+        continue
+      cond = 1-cond
+      continue
 
     if inst == "pass":
       if len(args) != 0:
@@ -198,9 +264,28 @@ def exec_stack(block, stack):
       if not args[0] in funs.keys():
         print("cannot call undefined function!")
         continue
-      stack = exec_stack(funs[args[0]][2], stack[:sp+1])
-      sp = len(stack) - 1
-      stack += [0.0] * (255 - len(stack))
+      if not cond == 1:
+        continue
+      fun = funs[args[0]]
+      type = fun[1]
+      if type == "!":
+        stack = exec_stack(fun[2], stack[:sp+1])
+        sp = len(stack) - 1
+        stack += [0.0] * (255 - len(stack))
+      elif type == "%":
+        vars["R"] = "_"
+        for i in range(fun[0]):
+          if sp < 0:
+            print("stack underflow! -> cant pass argument number " + str(i) + "!")
+            continue
+          vars["A"+str(i)] = stack[sp]
+          sp -= 1
+        exec(0, fun[2])
+        if vars["R"] != "_":
+          sp += 1
+          stack[sp] = vars["R"]
+      else:
+        print("Unsupported function execution type!")
       continue
 
     if inst == "icv":
